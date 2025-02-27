34d10de8cc2a105d8e870e416aaf2224602bbbd1
Bhupendra shakya <bhupendrashakya714@gmail.com>
2025-02-27 16:55:45 +0530

    [Bhupendra Shakya] Add . Setup of Address book application .

---
+++ src/test/java/com/Spring/AddressBokkApp/AddressBokkAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Spring.AddressBokkApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AddressBokkAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
